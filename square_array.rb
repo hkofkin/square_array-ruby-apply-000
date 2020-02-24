bacc1935fb9b435022852c18d26dcb2890ea7c9e
Hannah Kofkin <hkofkin@gmail.com>
2020-02-24 18:17:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ array = [1, 2, 3]
 def square_array(array)
   # your code here
   array.each do |square|
-    puts "#{square} * #{square}"
+    square *= square
   end
   return square_array(array)
 end
